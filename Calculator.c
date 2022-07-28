517c970414529006ad86ed97d331d4fa4deb2542
sagarbangade <sagarbangade21@gmail.com>
2022-07-28 08:39:59 +0530

    first comment

---
+++ Calculator.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b;
+    printf("Enter first number: ");
+    scanf("%d",&a);
+
+    printf("Enter second number: ");
+    scanf("%d",&b);
+
+
+    char c;
+    printf("Enter which type of calculation you want: ");
+    scanf(" %c",&c);
+    switch(c)
+    {
+    case '+':
+        printf("addition of %d + %d = %d",a,b,a+b);
+        break;
+    case '-':
+        printf("substraction of %d - %d = %d",a,b,a-b);
+        break;
+    case '*':
+        printf("multiplication of %d * %d = %d",a,b,a*b);
+        break;
+    case '/':
+        printf("division of %d / %d = %d",a,b,a/b);
+        break;
+
+    default:
+        printf("invalid");
+    }
+    return 0;
+}
